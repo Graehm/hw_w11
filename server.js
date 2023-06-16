5f935388277872dafc08c7392c38ae727dc594be
Graehm <gpatrellfazio@gmail.com>
2023-06-16 11:51:37 -0500

    server and app done but errors

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
 require('dotenv').config()
+
+const app = require('./app')
+const mongoose = require('mongoose')
+const PORT = process.env.PORT || 3000
+
+mongoose.connect(process.env.MONGO_URI)
+mongoose.connection.once('open', () => {
+    console.log('hey bud from mongo')
+})
+
+app.listen(PORT, () => {
+    console.log('Gotcha bud from PORT')
+})
