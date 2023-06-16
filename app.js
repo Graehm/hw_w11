661cd3863ee4ec31531342347b142b7ee987ef29
Graehm <gpatrellfazio@gmail.com>
2023-06-15 22:32:00 -0500

    controllers

--- app.js
+++ app.js
@@ -1,1 +1,2 @@
-app.use('/todos', userRoutes)
+app.use('/todos', todoRoutes)
+
--- controllers/toDoController.js
+++ controllers/toDoController.js
@@ -1,2 +1,47 @@
-//in todo routes -- use the exact names when naming the functions 
+require('dotenv').config()
+// const Todo = require('../models/todo')
 
+exports.getAllToDo = async (req, res) => {
+    try {
+        const foundTodos = await Todo.find({})
+        // res.render('todos/Index', {
+        //     todos: foundTodos
+        // })
+    } catch (error) {
+        res.status(400).json({message: error.message})
+    }
+}
+
+exports.createToDo = async (req, res) => {
+    try {
+        const {title, description} = req.body
+        const todo = new Todo({title, description})
+        await todo.save()
+    } catch (error) {
+        res.status(400).json({message: error.message})
+    }
+}
+
+exports.getToDo = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+exports.updateToDo = async (req, res) => {
+    try {
+        const foundTodo = await Todo.findOne({'_id': req.params.id})
+    } catch (error) {
+        res.status(400).json({message: error.message})
+    }
+}
+
+exports.deleteToDo = async (req, res) => {
+    try {
+        await Todo.findOneAndDelete({'_id': req.params.id})
+    } catch (error) {
+        res.status(400).json({message: error.message})
+    }
+}
