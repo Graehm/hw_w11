079deff5cb395e86587422cb9cadf04a606a294d
Graehm <gpatrellfazio@gmail.com>
2023-06-17 11:23:08 -0500

    updates

--- controllers/toDoController.js
+++ controllers/toDoController.js
@@ -4,9 +4,9 @@ const Todo = require('../models/todo')
 exports.getAllToDo = async (req, res) => {
     try {
         const foundTodos = await Todo.find({})
-        // res.render('todos/Index', {
-        //     todos: foundTodos
-        // })
+        res.json('todos/Index', {
+            todos: foundTodos
+        })
     } catch (error) {
         res.status(400).json({message: error.message})
     }
@@ -17,6 +17,7 @@ exports.createToDo = async (req, res) => {
         // const {title, description} = req.body
         const todo = new Todo(req.body)
         await todo.save()
+        res.json(req.body)
     } catch (error) {
         res.status(400).json({message: error.message})
     }
@@ -24,7 +25,10 @@ exports.createToDo = async (req, res) => {
 
 exports.getToDo = async (req, res) => {
     try {
-        
+        const foundTodo = await Todo.findOne({_id: req.body.id})
+        res.json(todo/Show, {
+            todo: foundTodo
+        })
     } catch (error) {
         res.status(400).json({message: error.message})
     }
