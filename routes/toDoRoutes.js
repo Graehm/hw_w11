8d705a0516680cb65ac624539672a4290b646a8f
Graehm <gpatrellfazio@gmail.com>
2023-06-29 23:42:04 -0500

    dudee

--- routes/toDoRoutes.js
+++ routes/toDoRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const todoController = require('../controllers/todoController')
 
 router.get('/', todoController.getAllToDo)
-router.post('/', todoController.createToDo)
+router.post('/create', todoController.createToDo)
 router.get('/:id', todoController.getToDo)
 router.put('/:id', todoController.updateToDo)
 router.delete('/:id', todoController.deleteToDo)
