e6ead15baccc1d5eb6484ec4465181b81748abdb
Graehm <gpatrellfazio@gmail.com>
2023-06-30 12:33:22 -0500

    done

--- tests/todo.test.js
+++ tests/todo.test.js
@@ -19,7 +19,7 @@ afterAll(async() => {
     server.close()
 })
 
-describe('Test todos endpoints', () => {
+describe('Test todos', () => {
     test('should create new post', async () => {
         const response = await request(app)
         .post('/todos')
@@ -30,8 +30,43 @@ describe('Test todos endpoints', () => {
         expect(response.body.description).toEqual("test desc.")
         expect(response.body.completed).toEqual(false)
     })
-    test('should return list of todos in array', () => {
-
+    test('should return all todos array', async () => {
+        const todos = [
+            {title: "test", describe: "test desc."},
+            {title: "test 2", description: "test desc. 2"}
+        ]
+        const response = await request(app).get("/todos")
+        expect(response.statusCode).toBe(200)
+        expect(response.body).toEqual(todos)
     })
+    test('shuold show a sinlge todo', async () => {
+        const todo = new Todo({title: "test", description: "test desc."})
+        await todo.save()
+        const response = await request(app)
+            .put(`/todos/${todo._id}`)
+            .send(todo)
 
+        expect(response.statusCode).toBe(200)
+        expect(response.title).toEqual('test')
+        expect(response.description).toEqual('test desc.')
+    })
+    test('should update a todo', async () => {
+        const todo = new Todo({title: "test", description: "test desc."})
+        await todo.save()
+        const response = await request(app)
+            .put(`/todos/${todo._id}`)
+            .send({title: "test", description: "test desc."})
+
+            expect(response.statusCode).toBe(200)
+            expect(response.title).toEqual('test')
+            expect(response.description).toEqual('test desc.')
+    })
+    test('should delete a todo', async () => {
+        const todo = new Todo ({title: "test", description: "test desc."})
+        await todo.save()
+        const response = await request(app).delete(`/todos/${todo._id}`)
+        
+        expect(response.statusCode).toBe(200)
+        expect(response.body._id).toEqual(todo._id.toString())
+    })
 })
